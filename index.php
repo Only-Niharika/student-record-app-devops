18e3917801ea597e5b6340b5bc4adcaa33c2548f
Only-Niharika <niharikakarkra06@gmail.com>
2026-06-04 15:46:53 +0000

    updated main heading

--- index.php
+++ index.php
@@ -18,9 +18,10 @@ function submitForm(rollno) {
 <body>
 <div class="container">
     <div class="header">
-        <h1>Student Records</h1>
+        <h1>Student Records App</h1>
         <p>Manage student details with a clean and modern interface</p>
 	<p>Made By - Niharika Karkra </p>
+	
    </div>
 
     <div class="card">
